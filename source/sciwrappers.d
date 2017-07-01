a087cb23b0171b3d26a98c542693280b3b685756
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-07-01 19:05:29 +0700

    sci_get_contents added

--- source/sciwrappers.d
+++ source/sciwrappers.d
@@ -5,6 +5,17 @@ import geany_plugin_d_api.scintilla.ScintillaWidget;
 
 extern(System) nothrow @nogc:
 
+/** Allocates and fills a buffer with text from the start of the document.
+ * @param sci Scintilla widget.
+ * @param buffer_len Buffer length to allocate, including the terminating
+ * null char, e.g. sci_get_length() + 1. Alternatively use @c -1 to get all
+ * text (since Geany 1.23).
+ * @return A copy of the text. Should be freed when no longer needed.
+ *
+ * @since 1.23 (0.17)
+ */
+gchar* sci_get_contents(ScintillaObject* sci, gint buffer_len);
+
 /** Gets the cursor position.
  * @param sci Scintilla widget.
  * @return Position. */
