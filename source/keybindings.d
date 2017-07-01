a754be0641b0fd6d9aac6b2c7873940e9f34c6b4
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-07-01 15:54:15 +0700

    extern added

--- source/keybindings.d
+++ source/keybindings.d
@@ -2,6 +2,8 @@ module geany_plugin_d_api.keybindings;
 
 import geany_plugin_d_api.types;
 
+extern(System) @nogc nothrow:
+
 struct GeanyKeyGroup;
 
 alias GeanyKeyGroupCallback = gboolean function(guint key_id);
--- source/pluginutils.d
+++ source/pluginutils.d
@@ -4,4 +4,6 @@ import geany_plugin_d_api.geanyplugin: GeanyPlugin;
 import geany_plugin_d_api.types;
 import geany_plugin_d_api.keybindings;
 
+extern(System) @nogc nothrow:
+
 GeanyKeyGroup* plugin_set_key_group(GeanyPlugin* plugin, const(gchar)* section_name, gsize count, GeanyKeyGroupCallback callback);
