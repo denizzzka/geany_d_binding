cf57d61d2e8307e133b87cf45fbccbb682a57f76
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-07-02 18:03:24 +0700

    tabs -> spaces, enum Msg added

--- source/scintilla/Scintilla.d
+++ source/scintilla/Scintilla.d
@@ -6,50 +6,66 @@ extern(System) @nogc nothrow:
 
 struct SCNotification
 {
-	Sci_NotifyHeader nmhdr;
-	Sci_Position position;
-	/* SCN_STYLENEEDED, SCN_DOUBLECLICK, SCN_MODIFIED, SCN_MARGINCLICK, */
-	/* SCN_NEEDSHOWN, SCN_DWELLSTART, SCN_DWELLEND, SCN_CALLTIPCLICK, */
-	/* SCN_HOTSPOTCLICK, SCN_HOTSPOTDOUBLECLICK, SCN_HOTSPOTRELEASECLICK, */
-	/* SCN_INDICATORCLICK, SCN_INDICATORRELEASE, */
-	/* SCN_USERLISTSELECTION, SCN_AUTOCSELECTION */
+    Sci_NotifyHeader nmhdr;
+    Sci_Position position;
+    /* SCN_STYLENEEDED, SCN_DOUBLECLICK, SCN_MODIFIED, SCN_MARGINCLICK, */
+    /* SCN_NEEDSHOWN, SCN_DWELLSTART, SCN_DWELLEND, SCN_CALLTIPCLICK, */
+    /* SCN_HOTSPOTCLICK, SCN_HOTSPOTDOUBLECLICK, SCN_HOTSPOTRELEASECLICK, */
+    /* SCN_INDICATORCLICK, SCN_INDICATORRELEASE, */
+    /* SCN_USERLISTSELECTION, SCN_AUTOCSELECTION */
 
-	int ch;
-	/* SCN_CHARADDED, SCN_KEY, SCN_AUTOCCOMPLETED, SCN_AUTOCSELECTION, */
-	/* SCN_USERLISTSELECTION */
-	int modifiers;
-	/* SCN_KEY, SCN_DOUBLECLICK, SCN_HOTSPOTCLICK, SCN_HOTSPOTDOUBLECLICK, */
-	/* SCN_HOTSPOTRELEASECLICK, SCN_INDICATORCLICK, SCN_INDICATORRELEASE, */
+    int ch;
+    /* SCN_CHARADDED, SCN_KEY, SCN_AUTOCCOMPLETED, SCN_AUTOCSELECTION, */
+    /* SCN_USERLISTSELECTION */
+    int modifiers;
+    /* SCN_KEY, SCN_DOUBLECLICK, SCN_HOTSPOTCLICK, SCN_HOTSPOTDOUBLECLICK, */
+    /* SCN_HOTSPOTRELEASECLICK, SCN_INDICATORCLICK, SCN_INDICATORRELEASE, */
 
-	int modificationType;	/* SCN_MODIFIED */
-	const(char)*text;
-	/* SCN_MODIFIED, SCN_USERLISTSELECTION, SCN_AUTOCSELECTION, SCN_URIDROPPED */
+    int modificationType;   /* SCN_MODIFIED */
+    const(char)*text;
+    /* SCN_MODIFIED, SCN_USERLISTSELECTION, SCN_AUTOCSELECTION, SCN_URIDROPPED */
 
-	Sci_Position length;		/* SCN_MODIFIED */
-	Sci_Position linesAdded;	/* SCN_MODIFIED */
-	int message;	/* SCN_MACRORECORD */
-	uptr_t wParam;	/* SCN_MACRORECORD */
-	sptr_t lParam;	/* SCN_MACRORECORD */
-	Sci_Position line;		/* SCN_MODIFIED */
-	int foldLevelNow;	/* SCN_MODIFIED */
-	int foldLevelPrev;	/* SCN_MODIFIED */
-	int margin;		/* SCN_MARGINCLICK */
-	int listType;	/* SCN_USERLISTSELECTION */
-	int x;			/* SCN_DWELLSTART, SCN_DWELLEND */
-	int y;		/* SCN_DWELLSTART, SCN_DWELLEND */
-	int token;		/* SCN_MODIFIED with SC_MOD_CONTAINER */
-	Sci_Position annotationLinesAdded;	/* SCN_MODIFIED with SC_MOD_CHANGEANNOTATION */
-	int updated;	/* SCN_UPDATEUI */
-	int listCompletionMethod;
-	/* SCN_AUTOCSELECTION, SCN_AUTOCCOMPLETED, SCN_USERLISTSELECTION, */
+    Sci_Position length;        /* SCN_MODIFIED */
+    Sci_Position linesAdded;    /* SCN_MODIFIED */
+    int message;    /* SCN_MACRORECORD */
+    uptr_t wParam;  /* SCN_MACRORECORD */
+    sptr_t lParam;  /* SCN_MACRORECORD */
+    Sci_Position line;      /* SCN_MODIFIED */
+    int foldLevelNow;   /* SCN_MODIFIED */
+    int foldLevelPrev;  /* SCN_MODIFIED */
+    int margin;     /* SCN_MARGINCLICK */
+    int listType;   /* SCN_USERLISTSELECTION */
+    int x;          /* SCN_DWELLSTART, SCN_DWELLEND */
+    int y;      /* SCN_DWELLSTART, SCN_DWELLEND */
+    int token;      /* SCN_MODIFIED with SC_MOD_CONTAINER */
+    Sci_Position annotationLinesAdded;  /* SCN_MODIFIED with SC_MOD_CHANGEANNOTATION */
+    int updated;    /* SCN_UPDATEUI */
+    int listCompletionMethod;
+    /* SCN_AUTOCSELECTION, SCN_AUTOCCOMPLETED, SCN_USERLISTSELECTION, */
 }
 
 struct Sci_NotifyHeader
 {
-	/* Compatible with Windows NMHDR.
-	 * hwndFrom is really an environment specific window handle or pointer
-	 * but most clients of Scintilla.h do not have this type visible. */
-	void *hwndFrom;
-	uptr_t idFrom;
-	uint code;
+    /* Compatible with Windows NMHDR.
+     * hwndFrom is really an environment specific window handle or pointer
+     * but most clients of Scintilla.h do not have this type visible. */
+    void *hwndFrom;
+    uptr_t idFrom;
+    uint code;
+}
+
+enum Msg : uint
+{
+    SCN_CHARADDED = 2001,
+    SCN_KEY = 2005,
+    SCN_UPDATEUI = 2007,
+    SCN_MODIFIED = 2008,
+    SCN_PAINTED = 2013,
+    SCN_AUTOCSELECTION = 2022,
+    SCN_AUTOCCANCELLED = 2025,
+    SCN_FOCUSIN = 2028,
+    SCN_FOCUSOUT = 2029,
+    SCN_AUTOCCOMPLETED = 2030,
+    SCI_AUTOCSHOW = 2100,
+    SCI_USERLISTSHOW = 2117
 }
